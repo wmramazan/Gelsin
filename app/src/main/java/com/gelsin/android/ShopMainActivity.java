fda21884ae5123c806988b9072f598c04b16683d
Ramazan VAPURCU <wmrmzn@gmail.com>
2018-02-17 15:08:51 +0300

    Added ShopMainActivity for owner of shops.

---
+++ app/src/main/java/com/gelsin/android/ShopMainActivity.java
@@ -0,0 +1,13 @@
+package com.gelsin.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShopMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_main);
+    }
+}
