9ba49210194d0f311fd81efcf416f2a77543fc87
Ramazan VAPURCU <wmrmzn@gmail.com>
2018-02-17 18:39:04 +0300

    Added new methods to GelsinActions.

--- app/src/main/java/com/gelsin/android/GelsinActions.java
+++ app/src/main/java/com/gelsin/android/GelsinActions.java
@@ -1,7 +1,11 @@
 package com.gelsin.android;
 
+import com.gelsin.android.item.ProductItem;
 import com.gelsin.android.util.ResultHandler;
+import com.google.gson.Gson;
 import com.loopj.android.http.RequestParams;
+
+import java.util.ArrayList;
 
 /**
  * Created by wmramazan on 17.02.2018.
@@ -27,16 +31,20 @@ public class GelsinActions {
         Gelsin.client.get("product/shop/" + shop_id, null, resultHandler);
     }
 
-    public static void getCustomerOrders() {
-
+    public static void getCustomerOrders(ResultHandler resultHandler) {
+        Gelsin.client.get("order/customer/" + CUSTOMER_ID, null, resultHandler);
     }
 
-    public static void getShopOrders() {
-
+    public static void getShopOrders(ResultHandler resultHandler) {
+        Gelsin.client.get("order/shop/" + SHOP_ID, null, resultHandler);
     }
 
-    public static void giveAnOrder() {
+    public static void giveAnOrder(String products, ResultHandler resultHandler) {
+        RequestParams params = new RequestParams();
+        params.put("customer", CUSTOMER_ID);
+        params.put("products", products);
 
+        Gelsin.client.post("order", params, resultHandler);
     }
 
 }
